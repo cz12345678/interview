68c836121df7bc7f73b47032ccbfb0026b7e9fd1
chenzhen-1 <1398620776@qq.com>
2019-08-08 20:40:59 +0800

    boot学习

---
+++ 01_代码/boot/spring-boot-01-helloworld/src/main/java/com/atguigu/HelloWorldMainApplication.java
@@ -0,0 +1,49 @@
+package com.atguigu;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
+
+
+/**
+ *  @SpringBootApplication 来标注一个主程序类，说明这是一个Spring Boot应用
+ */
+@SpringBootApplication
+//@ComponentScan(basePackages = { "com.atguigu.controller" })
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+
+        // Spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+
+
+    /**
+     * 查看 springboot 在启动的时候为我们注入了哪些 bean
+     * @param ctx
+     * @return
+     */
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+
+
+}
