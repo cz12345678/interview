9c1ff561fb4629681e7ef274c163bb16ad364dde
cz12345678 <1398620776@qq.com>
2020-02-17 18:10:28 +0800

    git学习笔记

---
+++ atguigu/尚硅谷Java数据结构和算法/代码/DataStructures/src/com/my/wechat/algorithm/Zdzys.java
@@ -0,0 +1,34 @@
+package com.my.wechat.algorithm;
+
+/**
+ * @ProjectName: DataStructures
+ * @Package: com.my.wechat.algorithm
+ * @ClassName: Zdzys
+ * @Author: chenzhen
+ * @Description: 最大质因数计算
+ * @Date: 2019/12/31 0031 下午 9:25
+ * @Version: 1.0
+ */
+public class Zdzys {
+
+    public static void main(String[] args) {
+        // 获取最大质因数
+//        System.out.println(getZdzys(13195)); // 5、7、13、29
+
+        System.out.println(getZdzys(120));
+
+    }
+
+    public static int getZdzys(int num){
+        if (num == 1){
+            return num;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0){
+                return getZdzys(num / i);
+            }
+        }
+        return num;
+    }
+
+}
